eb9192b65409c9f8f20894ea3e008b597e790ba3
Sergio Ramírez <sergioramirezsanmartin@gmail.com>
2022-12-12 13:00:00 -0300

    Modified main files App and Index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import freecodecampLogo from './imagenes/freecodecamp-logo.png'
+import ListaDeTareas from './componentes/ListaDeTareas.js';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="freecodecamp-logo-contenedor">
+                <img src={freecodecampLogo} className='freecodecamp-logo' alt='logo freecodecamp'/>
+            </div>
+            <div className='tareas-lista-principal'>
+                <h1>Mis Tareas</h1>
+                <ListaDeTareas/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
